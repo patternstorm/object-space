94464639791c8a1e7c796a882be4ae1630ec1553
Claudi Paniagua Macia <claudipmacia@gmail.com>
2018-03-06 19:18:24 +0100

    Add trait Particular inside Individuals module

--- app/model/Individuals.scala
+++ app/model/Individuals.scala
@@ -3,5 +3,6 @@ package model
 trait Individuals {
 
   trait Individual
+  trait Particular extends Individual
 
 }
---
+++ test/model/ParticularsSpec.scala
@@ -0,0 +1,17 @@
+package model
+
+import org.scalatestplus.play.PlaySpec
+
+class ParticularsSpec extends PlaySpec with Individuals {
+
+  "A Particular" must {
+    "be instantiable as a singleton" in {
+      object aParticular extends Particular
+    }
+    "be an Individual" in {
+      object aParticular extends Particular
+      val anIndividual: Individual = aParticular
+    }
+  }
+
+}
