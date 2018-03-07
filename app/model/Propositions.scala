cdb51f4b4e0eaa25331604394213bba14a8f412f
patternstorm <claudipmacia@gmail.com>
2018-03-07 22:19:01 +0100

    Add operators ∧, ∨, ⊃ and ! to create composite Propositions

--- app/model/Propositions.scala
+++ app/model/Propositions.scala
@@ -9,14 +9,24 @@ trait Propositions {
     def ⊃[P <: Proposition](p: P): this.type ∨ P = new ∨(this, p)
     def unary_! = new !(this)
   }
+
+  def ⊢[P <: Proposition](P: P)(implicit ev: P): P = ev
+
   case class is[X <: Individual, A <: Quality]() extends Proposition
   case class by[X <: Individual, Y <: Individual, R <: Relation]() extends Proposition
 
-  case class ∧[P1 <: Proposition, P2 <: Proposition](left: P1, right: P2) extends Proposition
-  case class ∨[P1 <: Proposition, P2 <: Proposition](left: P1, right: P2) extends Proposition
-  case class ⊃[P1 <: Proposition, P2 <: Proposition](left: P1, right: P2) extends Proposition
+  case class ∧[P <: Proposition, Q <: Proposition](left: P, right: Q) extends Proposition
+
+  case class ∨[P <: Proposition, Q <: Proposition](left: P, right: Q) extends Proposition
+
+  case class ⊃[P <: Proposition, Q <: Proposition](left: P, right: Q) extends Proposition
   case class ![P <: Proposition](arg: P) extends Proposition
 
-  def ⊢[P <: Proposition](p: P)(implicit fact: P): P = fact
+  object Proposition {
+    implicit def andIntro[P <: Proposition, Q <: Proposition](implicit P: P, Q: Q): P ∧ Q = new ∧(P, Q)
+
+    //implicit def orIntro[P <: Proposition, Q <: Proposition](implicit P: P): P ∨ Q = new ∨(P,Q)
+    implicit def modusPonens[P <: Proposition, Q <: Proposition](implicit R: P ⊃ Q, P: P): Q = R.right
+  }
 
 }
--- test/model/PropositionsSpec.scala
+++ test/model/PropositionsSpec.scala
@@ -4,39 +4,57 @@ import org.scalatestplus.play.PlaySpec
 
 class PropositionsSpec extends PlaySpec with Space {
 
-  "A Proposition" must {
-    "Combine with another Proposition using ∧ to yield another Proposition" in {
+  "A Proposition P" must {
+    "Combine with another Proposition Q, " +
+      "using ∧ to yield another Proposition P ∧ Q, " +
+      "which is true if both P and Q are true" in {
+      type x = x.type
       object x extends Particular
+      type y = y.type
       object y extends Particular
+      type A = A.type
       object A extends Quality
+      type B = B.type
       object B extends Quality
-      val p1 = x is A
-      val p2 = x is B
-      val p: Proposition = p1 ∧ p2
-      (p1 ∧ p2).left mustBe p1
-      (p1 ∧ p2).right mustBe p2
+      implicit object P extends (x is A)
+      implicit object Q extends (y is B)
+      val p: Proposition = P ∧ Q
+      (P ∧ Q).left mustBe P
+      (P ∧ Q).right mustBe Q
+      ⊢(P ∧ Q) mustBe p
     }
-    "Combine with another Proposition using ∨ to yield another Proposition" in {
+    "Combine with another Proposition Q, " +
+      "using ∨ to yield another Proposition P ∨ Q, " +
+      "which is true if P is true or Q is true" in {
+      type x = x.type
       object x extends Particular
       object y extends Particular
+      type A = A.type
       object A extends Quality
       object B extends Quality
-      val p1 = x is A
-      val p2 = x is B
-      val p: Proposition = (x is A) ∨ (x is B)
-      (p1 ∨ p2).left mustBe p1
-      (p1 ∨ p2).right mustBe p2
+      implicit object P extends (x is A)
+      val Q = y is B
+      val p: Proposition = P ∨ Q
+      (P ∨ Q).left mustBe P
+      (P ∨ Q).right mustBe Q
+      //⊢(P ∨ Q) mustBe p
     }
-    "Combine with another Proposition using ⇒ to yield another Proposition" in {
+    "Combine with another Proposition Q, " +
+      "using ⊃ to yield another Proposition P ⊃ Q, " +
+      "such that if P ⊃ Q is true and P is true then Q is true" in {
+      type x = x.type
       object x extends Particular
-      object y extends Particular
+      type A = A.type
       object A extends Quality
+      type B = B.type
       object B extends Quality
-      val p1 = x is A
-      val p2 = x is B
-      val p: Proposition = (x is A) ⊃ (x is B)
-      (p1 ∨ p2).left mustBe p1
-      (p1 ∨ p2).right mustBe p2
+      type P = P.type
+      implicit object P extends (x is A)
+      val Q = x is B
+      implicit object R extends (P ⊃ (x is B))(P, Q)
+      (P ⊃ Q).left mustBe P
+      (P ⊃ Q).right mustBe Q
+      ⊢(Q) mustBe Q
     }
     "Negatable to yield another proposition" in {
       object x extends Particular
